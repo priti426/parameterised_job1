42ba3fcce7830b14f812cd5af14f999ff738b911
priti426 <pritishelke7777@gmail.com>
2024-10-20 15:05:25 +0530

    hello

---
+++ src/main/java/com/example/parameterised_job/ParameterisedJobApplication.java
@@ -0,0 +1,13 @@
+package com.example.parameterised_job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParameterisedJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParameterisedJobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/parameterised_job/ParameterisedJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parameterised_job;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParameterisedJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
